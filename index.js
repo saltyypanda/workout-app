c46eabfb623ca374a1565cc9146b3b216d27f388
saltyypanda <esh7943@rit.edu>
2023-09-19 12:01:04 -0400

    Found out how to print chatgpt response (and just response) in console

--- index.js
+++ index.js
@@ -1,9 +1,46 @@
 require('dotenv').config();
+const axios = require('axios');
+
 const { OpenAI } = require("openai");
 
 const openai = new OpenAI({
   organization: "org-f2NApAOmy3mObCzhBPi4XHnw",
-  apiKey: process.env.OPENAI_API_KEY // This is also the default, can be omitted
+  apiKey: process.env.OPENAI_API_KEY
 });
 
-console.log(openai)
+
+async function getChatCompletion() {
+  const OPENAI_API_KEY = openai.apiKey;
+
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${OPENAI_API_KEY}`
+    }
+  };
+
+  const data = {
+    model: 'gpt-3.5-turbo',
+    messages: [
+      { role: 'user', content: 'Say this is a test!' }
+    ],
+    temperature: 0.7
+  };
+
+  try {
+    const response = await axios.post('https://api.openai.com/v1/chat/completions', data, config);
+    return response.data;
+  } catch (error) {
+    console.error('Error:', error);
+    throw error;
+  }
+}
+
+// Example usage
+getChatCompletion()
+  .then(result => {
+    console.log('Response:', result.choices[0].message.content);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
