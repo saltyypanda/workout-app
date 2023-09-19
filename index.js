85dc3cc2b5c598913195fd790ba4e1380d1f1033
saltyypanda <esh7943@rit.edu>
2023-09-19 11:40:25 -0400

    Added organization key to const openai

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const { OpenAI } = require("openai");
 
 const openai = new OpenAI({
+  organization: "org-f2NApAOmy3mObCzhBPi4XHnw",
   apiKey: process.env.OPENAI_API_KEY // This is also the default, can be omitted
 });
 
