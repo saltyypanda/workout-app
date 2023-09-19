523ad4c2f395309e765ee37ea348053ebd2046e7
saltyypanda <esh7943@rit.edu>
2023-09-19 14:45:44 -0400

    Added an openai webpack from github I found online. script is processing it

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,67 @@
+const path = require('path');
 const Dotenv = require('dotenv-webpack');
 
-module.exports = {
-  entry: './scripts/script.js', // Entry point file path
-  output: {
-    filename: 'bundle.js',
-    path: __dirname + '/scripts'
-  },
-  plugins: [
-    new Dotenv(),
-  ],
-};
+module.exports = [
+    {
+        // First configuration for openai.min.js
+        entry: './index.ts',
+        module: {
+            rules: [
+                {
+                    test: /\.ts?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
+        },
+        output: {
+            filename: 'openai.min.js',
+            path: path.resolve(__dirname, 'dist'),
+            globalObject: 'this',
+            library: {
+                name: 'OpenAI',
+                type: 'umd',
+            }
+        }
+    },
+    {
+        // Second configuration for openai.module.min.js
+        entry: './index.ts',
+        module: {
+            rules: [
+                {
+                    test: /\.ts?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.tsx', '.ts', '.js'],
+        },
+        output: {
+            filename: 'openai.module.min.js',
+            path: path.resolve(__dirname, 'dist'),
+            library: {
+                type: 'module',
+            },
+        },
+        experiments: {
+            outputModule: true
+        }
+    },
+    {
+        // Third configuration for your own script.js
+        entry: './scripts/script.js', // Entry point file path
+        output: {
+            filename: 'bundle.js',
+            path: __dirname + '/scripts'
+        },
+        plugins: [
+            new Dotenv(),
+        ],
+    }    
+];
