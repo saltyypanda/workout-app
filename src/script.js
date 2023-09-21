2b023694b4948f0eb71e96495d37f346c0d3ea48
saltyypanda <esh7943@rit.edu>
2023-09-20 20:37:28 -0400

    WE DID ITgit add --all Removed the onClick feature in the html. Added an eventListener instead in script.js, and it now calls the function sendMessage() on click!

--- src/script.js
+++ src/script.js
@@ -7,55 +7,59 @@ import axios from 'axios';
 import dotenv from 'dotenv';
 import { OpenAI } from 'openai';
 
-const openai = new OpenAI({
-  organization: "org-f2NApAOmy3mObCzhBPi4XHnw",
-  apiKey: dotenv.process.env.OPENAI_API_KEY
+document.getElementById('myButton').addEventListener('click', function() {
+  sendMessage(); // Assuming sendMessage() is defined in the same file
 });
 
+// const openai = new OpenAI({
+//   organization: "org-f2NApAOmy3mObCzhBPi4XHnw",
+//   apiKey: dotenv.process.env.OPENAI_API_KEY
+// });
 
-async function getChatCompletion() {
-  const OPENAI_API_KEY = openai.apiKey;
-  const config = {
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${OPENAI_API_KEY}`
-    }
-  };
 
-  const data = {
-    model: 'gpt-3.5-turbo',
-    messages: [
-      { role: 'user', content: 'Say this is a test!' }
-    ],
-    temperature: 0.7
-  };
+// async function getChatCompletion() {
+//   const OPENAI_API_KEY = openai.apiKey;
+//   const config = {
+//     headers: {
+//       'Content-Type': 'application/json',
+//       'Authorization': `Bearer ${OPENAI_API_KEY}`
+//     }
+//   };
 
-  try {
-    const response = await axios.post('https://api.openai.com/v1/chat/completions', data, config);
-    return response.data;
-  } catch (error) {
-    console.error('Error:', error);
-    throw error;
-  }
-}
+//   const data = {
+//     model: 'gpt-3.5-turbo',
+//     messages: [
+//       { role: 'user', content: 'Say this is a test!' }
+//     ],
+//     temperature: 0.7
+//   };
 
-async function sendMessage() {
-  // Get the user input from the input field
-  const userInput = document.getElementById('userInput').value;
-  const responseContainer = document.getElementById('responseContainer');
+//   try {
+//     const response = await axios.post('https://api.openai.com/v1/chat/completions', data, config);
+//     return response.data;
+//   } catch (error) {
+//     console.error('Error:', error);
+//     throw error;
+//   }
+// }
 
-  // Call getChatCompletion with user input
-  const result = await getChatCompletion();
-  alert(apiKey);
+export function sendMessage() {
+  // // Get the user input from the input field
+  // const userInput = document.getElementById('userInput').value;
+  // const responseContainer = document.getElementById('responseContainer');
+
+  // // Call getChatCompletion with user input
+  // const result = getChatCompletion();
+  alert("YOU DID IT!");
 }
 
 // Example usage
-getChatCompletion()
-  .then(result => {
-    // result = returns an array of choices, and each choice is what I think is a tuple of
-    // data such as message, and the message has other fields
-    console.log('Response:', result.choices[0].message.content);
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+// getChatCompletion()
+//   .then(result => {
+//     // result = returns an array of choices, and each choice is what I think is a tuple of
+//     // data such as message, and the message has other fields
+//     console.log('Response:', result.choices[0].message.content);
+//   })
+//   .catch(error => {
+//     console.error('Error:', error);
+//   });
